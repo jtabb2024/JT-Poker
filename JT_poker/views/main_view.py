c21150f4c4724ae547f31764adf8d2e75695a6d4
jtabb <jefftabb@gmail.com>
2024-05-17 09:37:57 -0400

    making message improvements

--- JT_poker/views/main_view.py
+++ JT_poker/views/main_view.py
@@ -1,16 +1,22 @@
+# main_view.py
 from django.shortcuts import render
 from .play_game import PlayGame
 from .spectate_game import SpectateGame
 from ..game_logic.message_tracker import MessageTracker
 
 def Start_game(request):
-    mtracker = MessageTracker.instance() # get the curent instance of Message_Tracker
-    mtracker.add_message("main_view is executing...")
+    mtracker = MessageTracker.instance()  # get the current instance of Message_Tracker
+    
+    # Initialize the messages list by retrieving it from the MessageTracker instance
+    messages = mtracker.get_messages()
+    
+    # Ensure the MessageTracker is initialized and the message is added once
+    if "Message Tracker Class is now running" not in messages:
+        mtracker.add_message("Message Tracker Class is now running")
+
     game_output = ""
-    messages = []
 
     if request.method == "POST":
-        print("Form submitted")  # Debug print
         if 'play' in request.POST:
             game = PlayGame()
             game_output = "Game initialized with human player."
@@ -20,7 +26,7 @@ def Start_game(request):
             game_output = "Spectator game initialized."
             mtracker.add_message(game_output)
 
-        # Retrieve messages from MessageTracker
+        # Retrieve updated messages from MessageTracker
         messages = mtracker.get_messages()
 
     context = {
